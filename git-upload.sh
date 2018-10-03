b758aba0d99b5f9cd559feecec104d5ec999dc3d
Simbaaa921 <42992981+Simbaaa921@users.noreply.github.com>
2018-10-03 10:31:35 +0300

    Update git-upload.sh

--- git-upload.sh
+++ git-upload.sh
@@ -1,7 +1,7 @@
 #1/bin/bash
 if [ $# == 1 ]
 then
-git config --global user.email machoeagle@gmail.com
+git config --global user.email simbabog@gmail.com
 git add .
 #git commit -m "20171012_09_055"
 git commit -m $1
