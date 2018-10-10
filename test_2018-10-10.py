92c2fd625a6580ee17ce67d65fac19261080ec75
Simbaaa921 <42992981+Simbaaa921@users.noreply.github.com>
2018-10-10 08:48:37 +0300

    Add files via upload

--- test_2018-10-10.py
+++ test_2018-10-10.py
@@ -1,5 +1,13 @@
 x = 5
-if x == 5:
+if x == 5 :
       print('Equals 5')
-if x > 4:
+if x > 4 :
    print('greater tjan 4')
+if x >= 4 :
+   print ('Greater then or Equals 5')
+if x <6 :
+   print('less then 6')
+if x <= 5 :
+   print ('Less than or Equals 5')
+if x != 6 :
+   print('Not equal 6')
