910c202397e573cdd16fff8151ffff795cdf5786
Simbaaa921 <42992981+Simbaaa921@users.noreply.github.com>
2018-10-31 11:08:02 +0200

    Add files via upload

---
+++ sin_caur_summu_ver1_c05.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from math import sin
+
+# (1.) *(2.)= (2.) - float * int = float
+# (1.) *(2.)= (2.) - float * float = float
+# float(input()) -> float
+x = float(input("Lietotajs, ludzu, ievadi argumentu (x): "))
+#print type(x)
+
+y = sin(x)
+print("sin(%.2f) = %.2f"%(x,y))
+
+a0 = (-1)**0*x**1/(1)
+S0 = a0
+print ("a0%.2f = %.2f"%(x,y))
